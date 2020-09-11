07be121d9b4c1edf40329c2ac45fae6848be80a4
Benjamin Degenhart <b.degenhart.work@gmail.com>
2020-09-11 17:48:01 +0200

    Bump the numbers up

--- FibonacciSpiral.js
+++ FibonacciSpiral.js
@@ -3,7 +3,7 @@ let y = 0;
 let angle = 0;
 let radius = 200;
 let outwards = 12;
-let n = 200;
+let n = 500;
 
 let goldenRatio = (1 + Math.sqrt(5)) / 2;
 let goldenAngle = 2 * Math.PI * (1 - 1 / goldenRatio); // https://en.wikipedia.org/wiki/Golden_angle
